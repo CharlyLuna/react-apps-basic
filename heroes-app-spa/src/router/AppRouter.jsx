c637e103c8343c7d6d0da80e82389a547f4c3057
CharlyLuna <cluna10@ucol.mx>
2023-08-15 21:29:36 -0600

    Add protected route

--- heroes-app-spa/src/router/AppRouter.jsx
+++ heroes-app-spa/src/router/AppRouter.jsx
@@ -3,13 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { HeroesRoutes } from '../heroes'
 import { HomePage } from '../home/HomePage'
+import { ProtectedRoute } from './ProtectedRoute'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='/*' element={<HomePage />} />
-        <Route path='/heroes/*' element={<HeroesRoutes />} />
+        <Route
+          path='/heroes/*'
+          element={
+            <ProtectedRoute>
+              <HeroesRoutes />
+            </ProtectedRoute>
+          }
+        />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
     </>
